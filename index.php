7d44dd3182f12b0d63f33baa2ee25a8447adacf7
Gustavo Costa <gustavobhweb@gmail.com>
2014-12-16 23:40:30 -0200

    casa

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 						<form method="post">
 							<input type="text" name="email" id="email" class="cr-input mail" placeholder="E-mail" />
 							<input type="password" name="senha" id="senha" class="cr-input pass" placeholder="Senha" />
-							<button type="submit" class="btn-login">ENTRAR ></button>
+							<button type="submit" class="btn-login">ENTRAR</button>
 						</form>
 					</div><!-- .login -->
 							
@@ -61,22 +61,122 @@
 			    <ul>
 			    	<?php 
 			    		for($i=1;$i<=54;$i++){
-			    	?>
-			        <li><a href="#"><img src="img/original/<?=$i?>.png" alt="Whatever works"/></a></li>
-			        <?php }?>
+			    		?>
+			        	<li><a href="#"><img src="img/original/<?=$i?>.png" alt="Whatever works"/></a></li>
+			        	<?php 
+			        	}
+			        ?>
 			    </ul>
 			</div>
 			<div class="left-anim-box">
 				<div class="anuncie">
-					<button class="btn-emprego">Quero emprego ></button>
+					<button class="btn-emprego">Quero emprego</button>
 				</div><!-- .anuncie -->
 			</div><!-- .left-anim-box -->
 			<div class="right-anim-box">
 				<div class="anuncie-vaga">
-					<button class="btn-contratar">Quero contratar ></button>
+					<button class="btn-contratar">Quero contratar</button>
 				</div><!-- .anuncie-vaga -->
 			</div><!-- .right-anim-box -->
 		</div><!-- .box-animado -->
+
+		<div class="passos">
+			<div class="main-content">
+				<div class="topo">
+					<img src="img/icons/arrow.png" />
+				</div><!-- .topo -->
+				<div class="content-box">
+					<div class='section'>
+						<h2>Quero emprego</h2>
+						<h3>Cadastre seu currículo</h3>
+						<div class="image solucao">
+							<img src="img/solucao-min.png" />
+						</div><!-- .image-solucao -->
+						<p>Você cadastra apenas uma vez, mas os benefícos serão usufruídos sempre que necessitar.</p>
+					</div><!-- .section -->
+					<div class='section'>
+						<h2>Receba novidades</h2>
+						<h3>Diversas empresas</h3>
+						<div class="image chances">
+							<img src="img/chances-min.png" />
+						</div><!-- .image-chances -->
+						<p>Receba vagas de emprego via e-mail, sistema e ainda convites por empresas que se interessaram pelo seu perfil.</p>
+					</div><!-- .section -->
+					<div class='section'>
+						<h2>Sucesso</h2>
+						<h3>Seu objetivo alcançado</h3>
+						<div class="image sucesso">
+							<img src="img/sucesso-min.png" />
+						</div><!-- .image-sucesso -->
+						<p>Diversas empresas com um mesmo objetivo, contratar novos talentos.</p>
+					</div><!-- .section -->
+					<div class="final-right">
+						<h2>Cadastre-se. É rápido.</h2>
+						<button class="btn-comece-agora">Comece agora</button>
+					</div><!-- .final-right -->
+					<div class="clear"></div>
+				</div><!-- .content-box -->
+			</div><!-- .main-content -->
+		</div><!-- .passos -->
+
+		<div class="recentes">
+			<div class="main-content">
+
+				<div class="left-set">
+					<img src="img/recentes-label.png" />
+
+					<div class="perfil-profissional">
+						<div class="title">
+							<h2>Perfil profissional:</h2>
+						</div><!-- title -->
+
+						<div class="item">
+							<div class="box"></div>
+							<h3>Monitor De Qualidade Iii - Zona Norte</h3>
+							<h5>Empresa com nome confidencial</h5>
+						</div><!-- .item -->
+						<div class="item">
+							<div class="box"></div>
+							<h3>Coordenador De Atendimento</h3>
+							<h5>Empresa com nome confidencial</h5>
+						</div><!-- .item -->
+						<div class="item">
+							<div class="box"></div>
+							<h3>Supervisor De Vendas</h3>
+							<h5>Empresa com nome confidencial</h5>
+						</div><!-- .item -->
+						<div class="item">
+							<div class="box"></div>
+							<h3>Monitor De Qualidade Iii - Zona Norte</h3>
+							<h5>Empresa com nome confidencial</h5>
+						</div><!-- .item -->
+
+						<div class="bottom">
+							<div class="dot on"></div>
+							<?php for ($i=0;$i<5;$i++) {?>
+							<div class="dot"></div>
+							<?php } ?>
+						</div><!-- .bottom -->
+
+					</div><!-- .perfil-profissional -->
+				</div><!-- .left-set -->
+				<div class="saia-na-frente">
+					<a href="#"><div class="saiba-na-frente"></div></a>
+				</div><!-- .saia-na-frente -->
+				<div class="clear"></div>
+			</div><!-- .main-content -->
+		</div><!-- .recentes -->
+
+		<div class="abrangencia">
+			<div class="main-content">
+				<div class="box">
+					<div class="infos">
+						<h1>Contrata Rápido tem abrangência nacional.</h1>
+						<h4>Plataforma 100% Brasileira.</h4>
+					</div><!-- .infos -->
+				</div><!-- .box -->
+			</div><!-- .main-content -->
+		</div><!-- .abrangencia -->
 
 		<div class="footer">
 			<div class="section1">
--- js/default.js
+++ js/default.js
@@ -3,6 +3,7 @@ $(function() {
 	$( '#ri-grid' ).gridrotator( {
 		rows: 4,
 		columns: 12,
+		maxStep: 15,
 		w320 : {
 			rows : 3,
 			columns : 4
